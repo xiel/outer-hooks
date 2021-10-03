83c1b340e57e4cb1d13dc247e6035236ba200cea
Felix Leupold <felix@xiel.de>
2021-10-03 11:53:02 +0200

    yarn install, format files

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'react-app',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'no-shadow': 'error',
+    'react/self-closing-comp': ['error', { component: true, html: true }],
+    'react/jsx-boolean-value': 'error',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'simple-import-sort/sort': 'error',
+    'import/first': 'warn',
+    'import/newline-after-import': 'warn',
+    'import/no-duplicates': 'warn',
+    'import/no-webpack-loader-syntax': 'warn',
+  },
+  settings: {
+    react: {
+      version: '999.999.999',
+    },
+  },
+}
